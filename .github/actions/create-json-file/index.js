83eb0122c2419542615379980a0f78b4b5d15622
Marcus Cuda <marcus@cuda.org>
2023-01-30 15:53:15 -0800

    adding workflow

---
+++ .github/actions/create-json-file/index.js
@@ -0,0 +1,28 @@
+const core = require('@actions/core');
+const glob = require('@actions/glob');
+const { readFile } = require('fs/promises')
+
+const create = async () => {
+    try {
+        const version = core.getInput('json-version');
+        core.debug("Version: " + version);
+        const patterns = ['**/*.json', '!**/node_modules/*', '!**/*.git*']
+        const globber = await glob.create(patterns.join('\n'));
+        const files = await globber.glob();
+
+        const json = {
+            meta: {
+                version
+            }, data: []
+        }
+        for (let i = 0; i < files.length; i++) {
+            const content = await readFile(files[i], 'utf8');
+            json.data.push(JSON.parse(content));
+        }
+        core.setOutput("data", JSON.stringify(json, null, 2));
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+create();
